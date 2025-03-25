356df8b2319d7d65f2f74a61d4f545ba7f2496e8
default <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-03-25 16:40:07 +0000

    init

--- answe.php
+++ answe.php
@@ -7,20 +7,17 @@
   <meta name="keywords" content="math, salary, PHP, income" />
   <meta name="author" content="Adrina Peighambarzadeh" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Salary in PHP</title>
-
   <link rel="stylesheet" href="./css/style.css" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.purple-deep_orange.min.css" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css" />
   <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
   <link rel="manifest" href="site.webmanifest" />
-
+  <title>Salary in PHP</title>
 </head>
 
 <body>
-
   <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
@@ -28,18 +25,16 @@
         <span class="mdl-layout-title">Salary in PHP</span>
       </div>
     </header>
-
     <main class="mdl-layout__content">
       <div class="right-image">
         <img src="./images/income_tax.jpg" alt="Income Tax Image" onerror="this.style.display='none'">
       </div>
-      
       <div class="page-content-php">
         <div id="user-info">
           <?php
           // tax rate
           const TAX_RATE = 0.18;
-
+          
           // input
           $hoursWorked = $_GET["hours-worked"];
           $hourlyWage = $_GET["hourly-wage"];
@@ -60,7 +55,5 @@
     </main>
   </div>
 </body>
-
-
 
 </html>
--- index.php
+++ index.php
@@ -7,8 +7,6 @@
   <meta name="keywords" content="math, salary, PHP, income" />
   <meta name="author" content="Adrina Peighambarzadeh" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Salary in PHP</title>
-
   <link rel="stylesheet" href="./css/style.css" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
   <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css" />
@@ -16,27 +14,25 @@
   <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
   <link rel="manifest" href="site.webmanifest" />
-
-  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <title>Salary in PHP</title>
 </head>
 
 <body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
         <span class="mdl-layout-title">Salary in PHP </span>
       </div>
     </header>
-
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="./images/income_tax.jpg" alt="Income Tax Image" onerror="this.style.display='none'">
+        <img src="./images/income_tax.jpg" alt="Income Tax Image" width=250px>
       </div>
       <br>
       <div class="page-content">
         <p>Please enter your employment information below:</p>
       </div>
-
       <form action="answer.php" method="GET">
         <div class="mdl-textfield mdl-js-textfield">
           <input class="mdl-textfield__input" type="number" id="hours-worked" name="hours-worked">
@@ -44,19 +40,16 @@
           <span class="mdl-textfield__error">Input is not a number!</span>
         </div>
         <br>
-
         <div class="mdl-textfield mdl-js-textfield">
           <input class="mdl-textfield__input" type="number" id="hourly-wage" name="hourly-wage">
           <label class="mdl-textfield__label" for="hourly-wage">Hourly wage ($)</label>
           <span class="mdl-textfield__error">Input is not a number!</span>
         </div>
         <br>
-
         <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" type="submit">
           Calculate
         </button>
       </form>
-
       <br>
       <div class="page-content-answer">
         <div id="TakeHomePay"></div>
